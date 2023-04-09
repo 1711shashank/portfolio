f8ac2343c618071f81a4bd69d3c8cab7f66dcd7c
1711shashank <1711shashank@gmail.com>
2023-04-10 00:18:16 +0530

    about

---
+++ src/Components/About.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './About.css';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+
+const About = () => {
+    return (
+        <>
+            <div className='about'>
+                <div className='about-left'>
+                    <img className='about-image' src={require('../assets/profile.png')} />
+                </div>
+                <div className='about-right'>
+                    <h4 className='about-right-head-text'> Who am I ? </h4>
+                    <h2 className='about-right-main-head'> About me </h2>
+                    <p className='about-right-location'> Kolkata, India | React JS | Node | Salesforce</p>
+                    <p className='about-right-aboutMe'> A Developer and A UI/UX Designer. Interested in coding, but not everything about me is designs and code snippets. I have a particular proclivity for playing the guitar and various other instruments, taking an interest in music composition and travelling. </p>
+                    <div class="about-resume-button">
+                        <a href={require("../assets/resume.pdf")} target="_blank">
+                            <span>Resume</span>
+                            <svg width="13px" height="10px" viewBox="0 0 15 10">
+                                <path d="M1,5 L11,5"></path>
+                                <polyline points="8 1 12 5 8 9"></polyline>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default About
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './Home';
 import './Body.css';
+import About from './About';
 
 
 const Body = () => {
   return (
     <div className='body'>
         <Home/>
+        <About/>
         
     </div>
   )

96280fe577b79906999ae3a31006d98e2c683f9c
1711shashank <1711shashank@gmail.com>
2023-04-11 01:25:58 +0530

    about for web

---
+++ src/Components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <>
+            <div className=''>
+                <img src={'https://i.pinimg.com/originals/d6/af/b6/d6afb6c5702631ed7e304d2ac40fb4f2.gif'} width='300px' />
+
+            </div>
+        </>
+    )
+}
+
+export default Services

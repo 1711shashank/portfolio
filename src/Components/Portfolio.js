95ba7cfbe721ca921c351f1860b13b3cf6317eb5
1711shashank <1711shashank@gmail.com>
2023-04-16 16:28:54 +0530

    task message

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -11,8 +11,8 @@ const Portfolio = () => {
                 <div className='portfolio-body'>
                     <ProjectCard cardSide={'Left'}  redirectURL={'https://clone-af8c1.web.app/'}  title={'E-commerce Website'} desc={'Amazon clone'} sourceImg={require("../assets/amazonClone.png") }/>
                     <ProjectCard cardSide={'Right'} redirectURL={'https://splitwise-client.onrender.com'}  title={'SplitWise'} desc={'Expense sharing'} sourceImg={require("../assets/splitwise.jpeg") }/>
-                    <ProjectCard cardSide={'Left'}  redirectURL={'https://shorturl-shashank.netlify.app'}  title={'Link Condenser'} desc={'Shorten long website links'} sourceImg={require("../assets/linkCondenser.png") }/>
-                    <ProjectCard cardSide={'Right'} redirectURL={'https://task-manager-e3i7.onrender.com'}  title={'Activity Manager'} desc={'Manager you every day Activity'} sourceImg={require("../assets/activityManager.png") }/>
+                    <ProjectCard cardSide={'Left'}  redirectURL={'https://shorturl-shashank.netlify.app'}  title={'Link Condenser'} desc={'Shorten long website links'} sourceImg={require("../assets/linkCondenser.jpeg") }/>
+                    <ProjectCard cardSide={'Right'} redirectURL={'https://task-manager-e3i7.onrender.com'}  title={'Task Manager'} desc={'Track your daily Tasks and activities'} sourceImg={require("../assets/activityManager.png") }/>
                 </div>
             </div>
         </>
